922c120d4648f12c28fd5be1bbcba0005d13d8db
TTOGEARII <cagameku3842@gmail.com>
2026-01-21 11:37:53 +0900

    default rotue init

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//메인
+Route::get("/",[MainController::class,"index"]);
+
+//오타쿠샵
+Route::prefix("otaku-shop")->group(base_path("routes/otaku-shop.php"));
